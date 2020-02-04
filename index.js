16798c4042c05b26e9bf2118158751825f5cf6ad
joneshshrestha <contact@joneshshrestha.com>
2020-02-04 13:17:36 +0545

    update

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const telegram = require("telegram-bot-api");
+const APIkey = require("./api_key");
 
 const api = new telegram({
-  token: "1053860461:AAE73_cwTbnyydjQygTeyiOMWMAhmqe5Ja4"
+  token: APIkey
 });
 
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,45 @@ app.use(
 //This is the route the API will call
 app.post("/", function(req, res) {
   const { message } = req.body;
+
+  // app.post("/", function(req, res) {
+  //   // console.log(req.body.crypto)
+  //   let crypto = req.body.crypto;
+  //   let fiat = req.body.fiat;
+  //   let amount = req.body.amount;
+
+  //   let options = {
+  //     url: "https://apiv2.bitcoinaverage.com/convert/global",
+  //     method: "GET",
+  //     qs: {
+  //       from: crypto,
+  //       to: fiat,
+  //       amount: amount
+  //     }
+  //   };
+
+  //   request(options, function(error, response, body) {
+  //     let data = JSON.parse(body);
+  //     let price = data.price;
+  //     console.log(price);
+
+  //     let currentDate = data.time;
+
+  //     res.write("<p> The current date is " + currentDate + "</p>");
+  //     res.write(
+  //       "<h1 align='center' style='margin:330px 500px;'>" +
+  //         amount +
+  //         " " +
+  //         crypto +
+  //         " is currently worth " +
+  //         price +
+  //         " " +
+  //         fiat +
+  //         " <h1>"
+  //     );
+  //     res.send();
+  //   });
+  // });
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
